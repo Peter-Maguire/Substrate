c455110e99060588e95e36c467235941b1d6563a
UnacceptableUse <unacceptableuse@live.co.uk>
2013-05-11 21:41:45 +0100

    Added more screen tools

--- game/screen/ScreenTools.java
+++ game/screen/ScreenTools.java
@@ -13,7 +13,7 @@ public class ScreenTools {
 		g.fillRect(x, y, length, height);
 		g.setColor(Color.black);
 		g.drawRect(x-1, y-1, length+1, height+1);
-		game.getFontRenderer().drawString(text, x+(length/text.length()), y+5, 2);
+		game.getFontRenderer().drawString(text, x+(length/text.length())/2, y+5, 2);
 	}
 	
 	public static void drawButton(int x, int y, int length, int height, String text, Graphics g, Game game, Color foreground, Color background)
@@ -22,6 +22,21 @@ public class ScreenTools {
 		g.fillRect(x, y, length, height);
 		g.setColor(background);
 		g.drawRect(x-1, y-1, length+1, height-1);
-		game.getFontRenderer().drawString(text, x+(length/text.length()), y+5, 2);
+		game.getFontRenderer().drawString(text, x+(length/text.length())/2, y+5, 2);
+	}
+	
+	public static void drawOnOffButton(int x, int y, int length, int height, String on, Graphics g, Game game)
+	{
+		ScreenTools.drawButton(x, y, length, height, on, g, game, new Color(on == "ON" ? 0 : 145,on == "ON" ? 145 : 0,0,155), on=="ON" ? Color.green : Color.red);
+	}
+	
+	public static void drawProgressBar(int x, int y, int length, int height, int amount, Graphics g, Game game, Color foreground, Color background)
+	{
+		g.setColor(foreground);
+		g.fillRect(x, y, length, height);
+		g.setColor(background);
+		g.drawRect(x-1, y-1, length+1, height-1);
+		g.fillRect(x+1, y+1, (length/100*amount)-2, height-4);
+
 	}
 }
