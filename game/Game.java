e61f1e79377586b956ede3bef3010113f905b09e
UnacceptableUse <unacceptableuse@live.co.uk>
2013-05-13 18:15:41 +0100

    Cleaned up

--- game/Game.java
+++ game/Game.java
@@ -31,7 +31,7 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 
 	private static final long serialVersionUID = 1L;
 	public static String TITLE = "Substrate";
-	public static int WIDTH = 800, HEIGHT = 600, SCALE = 2, SIZE = SCALE *  32;
+	public static int WIDTH = 800, HEIGHT = 600, SCALE = 2, SIZE = SCALE * 32;
 	public HashMap<String, String> SETTINGS = new HashMap<String, String>();
 
 	public BufferStrategy strategy;
@@ -53,14 +53,13 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 	public boolean gameRunning = true;
 
 	public Game() {
-  
-		setSize(200,200);
+
+		setSize(200, 200);
 		setBounds(0, 0, 800, 600);
 		setIgnoreRepaint(true);
 		addKeyListener(this);
 		addFocusListener(this);
 		addMouseListener(this);
-		
 
 	}
 
@@ -76,24 +75,21 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 		container.setResizable(true);
 		container.setVisible(true);
 		game.start();
-		
 
 	}
-	
-
 
 	public synchronized void start() {
-
-	
 
 		try {
 			gameLoop();
 		} catch (Exception e) {
-			e.printStackTrace();	
-			String[] crashDump = {currentScreen.toString(), Integer.toString(fps), Integer.toString(tps), e.getMessage()};
+			e.printStackTrace();
+			String[] crashDump = { currentScreen.toString(),
+					Integer.toString(fps), Integer.toString(tps),
+					e.getMessage() };
 			setScreen(new ScreenLoading(WIDTH, HEIGHT, null, crashDump));
 			render();
-			
+
 		}
 		requestFocus();
 
@@ -103,8 +99,8 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 		gameRunning = false;
 		System.exit(ABORT);
 	}
-	public void shutdown()
-	{
+
+	public void shutdown() {
 		gameRunning = false;
 		log("Saving files...");
 		FileSaver.save(SETTINGS, "settings.dat");
@@ -114,9 +110,10 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 		log("Shutting down game, peace.");
 		try {
 			Thread.sleep(1000L);
-		} catch (InterruptedException e){}
+		} catch (InterruptedException e) {
+		}
 		System.exit(0);
-		
+
 	}
 
 	public Font getFontRenderer() {
@@ -128,16 +125,15 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 
 		createBufferStrategy(2);
 		strategy = getBufferStrategy();
-		
-		/*ThreadConsole tc = new ThreadConsole();
-		Thread consoleThread = new Thread(tc);
-		consoleThread.run();
-		console = tc.console;*/
+
+		/*
+		 * ThreadConsole tc = new ThreadConsole(); Thread consoleThread = new
+		 * Thread(tc); consoleThread.run(); console = tc.console;
+		 */
 		log("Loading...");
-		
+
 		File f = new File("settings.dat");
-		if(!f.exists())
-		{  
+		if (!f.exists()) {
 			log("Settings file does not exist!");
 			SETTINGS.put("Sound", "OFF");
 			SETTINGS.put("Debug", "ON");
@@ -146,23 +142,18 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 			SETTINGS.put("Cheats", "OFF");
 			SETTINGS.put("HasDoneIntro", "OFF");
 			FileSaver.save(SETTINGS, "settings.dat");
-		}else
-		{
+		} else {
 			log("Found settings.dat, loading...");
 			SETTINGS = (HashMap<String, String>) FileSaver.load("settings.dat");
 		}
-		
+
 		settings = new Options(SETTINGS);
 		SETTINGS.put("Debug", "ON");
-		
-		f = new File(FileSaver.getCleanPath()+"\\maps\\");
-		if(!f.exists())
-		{
-			log("Creating directory "+f.getAbsolutePath());
+
+		f = new File(FileSaver.getCleanPath() + "\\maps\\");
+		if (!f.exists()) {
+			log("Creating directory " + f.getAbsolutePath());
 			f.mkdirs();
-		}else
-		{
-			log("Map folder already exists");
 		}
 		tileinit = new Tile();
 
@@ -176,14 +167,13 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 		} catch (Exception e) {
 			log("Sheet loading failed!");
 			throw new RuntimeException("Sheet loading failed!");
-		}	
+		}
 		f = new File("keymap.dat");
-		if(!f.exists())
-		{  
+		if (!f.exists()) {
 			FileSaver.save(controls.keyMap, "keymap.dat");
-		}else
-		{
-			controls.keyMap = (HashMap<Integer, Integer>) FileSaver.load("keymap.dat");
+		} else {
+			controls.keyMap = (HashMap<Integer, Integer>) FileSaver
+					.load("keymap.dat");
 		}
 
 		soundman = new SoundManager(this);
@@ -191,14 +181,11 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 		setScreen(new ScreenMainMenu(WIDTH, HEIGHT, sheet));
 		log("Loaded!");
 	}
-	
-	public static void log(String s)
-	{
-		if(console != null)
-		{
+
+	public static void log(String s) {
+		if (console != null) {
 			console.log(s);
-		}else
-		{
+		} else {
 			System.out.println(s);
 		}
 	}
@@ -222,14 +209,10 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 				ticks++;
 				tick();
 				unprocessed -= 1;
-
 			}
-			{
 				frames++;
 				render();
-				revalidate();
-			}
-
+				revalidate();	
 			if (System.currentTimeMillis() - lastTimer1 > 1000) {
 				lastTimer1 += 1000;
 
@@ -237,103 +220,65 @@ public class Game extends Canvas implements KeyListener, MouseListener,
 				tps = ticks;
 				ticks = 0;
 				frames = 0;
-
 			}
-
 		}
 		System.err.println("Game stopped :(");
 		shutdown();
 	}
-
 	public void tick() {
 		currentScreen.tick();
-	
-		
-
 	}
-
 	public void setScreen(Screen screen) {
 		currentScreen = screen;
 		currentScreen.init(this);
 	}
-	
-
-
 	public void render() {
-	
 		g = (Graphics2D) strategy.getDrawGraphics();
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH * 2, HEIGHT * 2);
 		currentScreen.render(g);
 		if (settings.getSetting("Debug") == "ON")
 			font.drawString(tps + " Ticks, " + fps + " fps", 1, 1, 2);
-		if(settings.getSetting("Cheats") == "ON")
-		{
+		if (settings.getSetting("Cheats") == "ON") {
 			font.drawString("Cheat Menu:", 660, 10, 1);
 			font.drawString("x - xxxxxx", 655, 25, 1);
 		}
-
 		g.dispose();
 		strategy.show();
 	}
-
 	@Override
 	public void focusGained(FocusEvent arg0) {
 		currentScreen.focusGained(arg0);
-
 	}
-
 	@Override
 	public void focusLost(FocusEvent arg0) {
 		currentScreen.focusLost(arg0);
-
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
-	
-	}
-	
-
-
+	public void mouseClicked(MouseEvent e) {}
 	@Override
-	public void mouseEntered(MouseEvent e) {
-
-	}
-
+	public void mouseEntered(MouseEvent e) {}
 	@Override
-	public void mouseExited(MouseEvent e) {
-
-	}
-
+	public void mouseExited(MouseEvent e) {}
 	@Override
 	public void mousePressed(MouseEvent e) {
 		currentScreen.mousePressed(e);
-
 	}
-
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		currentScreen.mouseReleased(e);
-
 	}
-
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		currentScreen.keyPressed(arg0);
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		currentScreen.keyReleased(arg0);
-
 	}
-
 	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
+	public void keyTyped(KeyEvent arg0) {}
 
 }
