cb48621c135a24ae382f800e2380f84f3289610d
UnacceptableUse <unacceptableuse@live.co.uk>
2013-05-29 15:29:31 +0100

    Added ToolReplace

---
+++ game/mapeditor/tools/ToolReplace.java
@@ -0,0 +1,18 @@
+package game.mapeditor.tools;
+
+import game.screen.ScreenMapEditor;
+
+public class ToolReplace extends Tool{
+
+	public ToolReplace(String toolName, int sprite) {
+		super(toolName, sprite);
+		
+	}
+	
+	@Override
+	public void onToolUsed(int x, int y, ScreenMapEditor screen)
+	{
+		
+	}
+
+}
