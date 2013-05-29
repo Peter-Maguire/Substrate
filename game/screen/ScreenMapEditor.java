52ba2fb7ac66ec2c13c83a09bf0285bdbe36ad88
UnacceptableUse <unacceptableuse@live.co.uk>
2013-05-29 15:30:14 +0100

    Added menus to map editor

--- game/screen/ScreenMapEditor.java
+++ game/screen/ScreenMapEditor.java
@@ -5,6 +5,7 @@ import game.SpriteSheet;
 import game.entity.Entity;
 import game.mapeditor.tools.Tool;
 import game.mapeditor.tools.ToolPencil;
+import game.mapeditor.tools.ToolReplace;
 import game.tile.Tile;
 
 import java.awt.Color;
@@ -25,6 +26,7 @@ public class ScreenMapEditor extends Screen {
 	private HashMap<Rectangle, Tile> tiles = new HashMap<Rectangle, Tile>();
 	private ArrayList<Entity> entities = new ArrayList<Entity>();
 	private int openMenu = 0;
+	private boolean isPlacingTile = true;
 	
 	private Tool currentTool = null;
 	private Tile currentTile = null;
@@ -43,11 +45,31 @@ public class ScreenMapEditor extends Screen {
 	
 	private void init()
 	{
-		toolRegistry.clear();
 		toolRegistry.add(new ToolPencil("Pencil", 1));
+		toolRegistry.add(new ToolReplace("Replacer", 0));
 		addButton("selectTile", new Rectangle(10, 520, 64, 64));
 		addButton("selectTool", new Rectangle(104, 520, 64, 64));
 		
+		int i = 0;
+		for(Tool t : toolRegistry){
+			addButton(""+i, new Rectangle(112+(42*i), 434, 32, 32));
+			i++;
+		}
+		
+		i = 0;
+		int x = 0;
+		int y = 0;
+		for(Tile t : Tile.tiles){
+			addButton(""+i, new Rectangle(14+(42*x),227+(42*y),32,32));
+			i++;
+			x++;
+			if(45*x > 400)
+			{
+				y++;
+				x = 0;
+			}
+		}
+
 		currentTool = toolRegistry.get(0);
 		currentTile = Tile.tiles[1];
 	}
@@ -74,10 +96,6 @@ public class ScreenMapEditor extends Screen {
 	}
 	
 	private void drawMenuBox(int x, int y, int width, int height, Graphics g){
-		g.setColor(Color.white);
-		g.drawRect(x, y+height/2, 64, height/2);
-		g.drawRect(x, y-height/2, width, height);
-		
 		g.setColor(new Color(255,255,255,155));
 		g.fillRect(x, y+height/2, 64, height/2);
 		g.fillRect(x, y-height/2, width, height);
@@ -90,19 +108,72 @@ public class ScreenMapEditor extends Screen {
 		g.fillRect(0, 500, Game.WIDTH, 100);
 		drawTileSelection(10, 520, currentTile.sprite,"Tile", g);
 		drawToolSelection(104, 520, currentTool.getSprite(),"Tool", g);
-		drawMenuBox(10,320,400,200, g);
+		if(openMenu == MENU_TILE)
+		{
+			drawMenuBox(10,320,400,200, g);
+			int i = 0;
+			int x = 0;
+			int y = 0;
+			for(Tile t : Tile.tiles){
+				g.setColor(Color.BLACK);
+				g.drawRect(13+(42*x), 225+(42*y), 33, 34);
+				g.setColor(new Color(0,0,0,135));
+				g.fillRect(13+(42*x), 225+(42*y), 33, 34);
+				g.drawImage(game.sheetTiles.getImage(Tile.tiles[i].sprite),14+(42*x),227+(42*y),32,32,game);
+				i++;
+				x++;
+				if(45*x > 400)
+				{
+					y++;
+					x = 0;
+				}
+			}
+			
+			
+		}
+		if(openMenu == MENU_TOOL)
+		{
+			drawMenuBox(104,450,300,50, g);
+			int i = 0;
+			for(Tool t : toolRegistry){
+				g.setColor(Color.BLACK);
+				g.drawRect(112+(42*i), 434, 32, 32);
+				g.setColor(new Color(0,0,0,135));
+				g.fillRect(112+(42*i), 434, 32, 32);
+				g.drawImage(game.sheetUI.getImage(t.getSprite()),112+(42*i),436,32,32,game);
+				i++;
+			}
+		}
+		
 
 	}
-	
-	
-	
-	
+
 	@Override
 	public void postAction(String name)
 	{
-		if(name == "selectTile")openMenu = MENU_TILE;
-		
-		if(name == "selectTool")openMenu = MENU_TOOL;
+		isPlacingTile = false;
+		if(name == "selectTile")
+		{
+			openMenu = openMenu == MENU_TILE ? MENU_NONE : MENU_TILE;
+			return;
+		}
+		if(name == "selectTool")
+		{
+			openMenu = openMenu == MENU_TOOL ? MENU_NONE : MENU_TOOL;
+			return;
+		}
+		if(openMenu == MENU_TOOL)
+		{
+			currentTool = toolRegistry.get(Integer.parseInt(name));
+			openMenu = MENU_NONE;
+			return;
+		}
+		if(openMenu == MENU_TILE)
+		{
+			currentTile = Tile.tiles[Integer.parseInt(name)];
+			openMenu = MENU_NONE;
+		}
+
 	}
 
 	
