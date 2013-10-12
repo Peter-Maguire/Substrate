3e92c9942478642493635d719ddd583916af6c45
UnacceptableUse <unacceptableuse@gmail.com>
2013-10-12 13:21:15 +0100

    Fixed tile array not being formatted correctly

--- game/tile/Tile.java
+++ game/tile/Tile.java
@@ -9,8 +9,11 @@ public class Tile implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -2215788549581724737L;
-	public static Tile[] tiles = { new TileNoTile(), new TileGrass(),
-			new TilePaving(), new TileWall(TileWall.BOTTOM_CORNER_LEFT),
+	public static Tile[] tiles = { 
+			new TileNoTile(),
+			new TileGrass(),
+			new TilePaving(),
+			new TileWall(TileWall.BOTTOM_CORNER_LEFT),
 			new TileWall(TileWall.BOTTOM_CORNER_RIGHT),
 			new TileWall(TileWall.TOP_CORNER_LEFT),
 			new TileWall(TileWall.TOP_CORNER_RIGHT),
@@ -19,8 +22,10 @@ public class Tile implements Serializable {
 			new TileWall(TileWall.WALL_HORIZONTAL_RIGHT),
 			new TileWall(TileWall.WALL_VERTICAL),
 			new TileWall(TileWall.WALL_VERTICAL_BOTTOM),
-			new TileWall(TileWall.WALL_VERTICAL_TOP), new TileWater(),
-			new TileNoTile(), new TileRubble(TileRubble.RUBBLE_VERTICAL_TOP),
+			new TileWall(TileWall.WALL_VERTICAL_TOP),
+			new TileWater(),
+			new TileNoTile(),
+			new TileRubble(TileRubble.RUBBLE_VERTICAL_TOP),
 			new TileRubble(TileRubble.RUBBLE_VERTICAL_MIDDLE),
 			new TileRubble(TileRubble.RUBBLE_VERTICAL_BOTTOM),
 			new TileRubble(TileRubble.RUBBLE_TOP_CORNER_LEFT),
