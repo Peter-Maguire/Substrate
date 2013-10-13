984de6397f1c5b508e5be365aeec9a76b0182957
UnacceptableUse <unacceptableuse@gmail.com>
2013-10-13 14:13:36 +0100

    Added Dispenser tiles

---
+++ game/tile/TileDispenser.java
@@ -0,0 +1,10 @@
+package game.tile;
+
+public class TileDispenser extends Tile
+{
+
+	public TileDispenser(int rotation, boolean active)
+	{
+		this.sprite = active ? 41 + rotation : 25 + rotation;
+	}
+}
